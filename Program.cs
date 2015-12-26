a594572b7f6a7e38817f7aef6c2eae1b472114a7
wangjianlong <810844480@qq.com>
2015-12-26 12:40:30 +0800

    commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using DNA.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DNA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //MDBHelper.ReadOne("Select XZJDMC from YDDW GROUP BY XZJDMC");
+            var list = MDBHelper.GetAllDistrict();
+            if (list.Count != 0)
+            {
+                var dict = list.GetExcelOneData();
+                Console.ReadLine();
+
+            }
+           
+            Console.ReadLine();
+        }
+    }
+}
